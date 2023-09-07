13d3e69f221319ed3a86935a44da82e76516d79d
Kareem Amara <kareemamarah2356@gmail.com>
2023-09-07 14:51:46 +0300

    adjustline in accordion

--- js/myAccordionBtn.js
+++ js/myAccordionBtn.js
@@ -1,15 +1,17 @@
 const accordionItems = document.querySelectorAll(".my-accordion-item");
 const accordionItem = document.querySelector(".my-accordion-item");
 accordionItems.forEach((item) => {
+  const createLine = document.createElement("div");
   item.addEventListener("click", () => {
-    const createLine = document.createElement("div");
     item.style.cssText =
       "box-shadow: none !important;border: none !important; outline: none;border-right-color: none !important;border-left-color: none !important; border-top-color: none !important;border-bottom-color: none !important;";
-    item.append(createLine);
+
     accordionItem.style.cssText = "position: relative;overflow:hidden;";
     createLine.style.cssText =
-      "background-color: #DAA520;position: absolute;width: 5px;height: 100%;left:4.5%";
+      "background-color: #DAA520;position: absolute;width: 5px;height: 100%;left:3.5%";
+    createLine.classList.add("accordion-center-line");
   });
+  item.append(createLine);
 });
 
 // function showLine() {
