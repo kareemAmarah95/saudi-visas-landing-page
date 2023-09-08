91f018a8ed3ab4aa8fef9d21684d8158f65fc7a9
Kareem Amara <kareemamarah2356@gmail.com>
2023-09-08 12:07:33 +0300

    center the accordion-center-line

--- js/myAccordionBtn.js
+++ js/myAccordionBtn.js
@@ -2,15 +2,13 @@ const accordionItems = document.querySelectorAll(".my-accordion-item");
 const accordionItem = document.querySelector(".my-accordion-item");
 accordionItems.forEach((item) => {
   const createLine = document.createElement("div");
-  item.addEventListener("click", () => {
-    item.style.cssText =
-      "box-shadow: none !important;border: none !important; outline: none;border-right-color: none !important;border-left-color: none !important; border-top-color: none !important;border-bottom-color: none !important;";
+  item.style.cssText =
+    "box-shadow: none !important;border: none !important; outline: none;border-right-color: none !important;border-left-color: none !important; border-top-color: none !important;border-bottom-color: none !important;";
 
-    accordionItem.style.cssText = "position: relative;overflow:hidden;";
-    createLine.style.cssText =
-      "background-color: #DAA520;position: absolute;width: 5px;height: 100%;left:3.5%";
-    createLine.classList.add("accordion-center-line");
-  });
+  accordionItem.style.cssText = "position: relative;";
+  createLine.style.cssText =
+    "background-color: #DAA520;position: absolute;width: 5px;height: 100%;left:3.5%; top: 10%";
+  createLine.classList.add("accordion-center-line");
   item.append(createLine);
 });
 
